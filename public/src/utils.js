c357195063d2037fafa5ea571d1adee504f5c07d
wbworkman <2686218021@qq.com>
2017-11-05 21:27:48 +0800

    完成了登录功能

---
+++ public/src/utils.js
@@ -0,0 +1,11 @@
+define(['jquery'], function ($) {
+    $.ajax({
+        url: '/api/employee/checkRootLogin',
+        type: 'get',
+        success: function (info) {
+            if (info.error) {
+                location.href = '/login.html';
+            }
+        }
+    });
+})
